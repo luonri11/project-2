df545b9a26d0793983f9075fca81aada88ab813e
Berta Devenyi <berta.j.devenyi@gmail.com>
2022-07-30 11:01:36 +1000

    Added one more query, not perfect yet

--- Berta_queries/queries.sql
+++ Berta_queries/queries.sql
@@ -5,3 +5,12 @@ LEFT JOIN melb_data ON melbourne_venues.suburb = melb_data.suburb
 	and melbourne_venues.latitude = melb_data.lattitude  
 	and melbourne_venues.longitude = melb_data.longtitude;
 
+-- Look for venues where price is less than $2M and has more than 4 rooms
+SELECT melbourne_venues.venue, melbourne_venues.category, melbourne_venues.suburb, melb_data.price
+FROM melbourne_venues
+LEFT JOIN melb_data ON melbourne_venues.suburb = melb_data.suburb 
+	and melbourne_venues.latitude = melb_data.lattitude  
+	and melbourne_venues.longitude = melb_data.longtitude
+where melb_data.price < '2000000' and melb_data.rooms > 4	
+	
+	;
